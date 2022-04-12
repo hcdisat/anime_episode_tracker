915fd7075c1522ee92120a5c5a6136418b3272ee
Hector Caraballo <hcdisat@gmail.com>
2022-04-11 22:40:24 -0400

    adding unit tests

---
+++ app/src/test/java/com/hcdisat/animetracker/viewmodels/AnimeViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.hcdisat.animetracker.viewmodels
+
+import junit.framework.TestCase
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class AnimeViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun `it should call getTrending and return LOADING state`() {
+
+    }
+}
